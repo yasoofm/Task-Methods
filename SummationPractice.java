09b1262ddfbcda84d122d6e8c038fa19d4aad533
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-03 10:50:08 +0300

    submission

---
+++ SummationPractice.java
@@ -0,0 +1,16 @@
+public class SummationPractice{
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>(List.of(1,2,3,4,5));
+    
+        int result = sum(numbers);
+        System.out.println(result);
+      }
+      
+      public static int sum(ArrayList<Integer> nums){
+        int result = 0;
+        for(int num : nums){
+          result += num;
+        }
+        return result;
+      }
+}
